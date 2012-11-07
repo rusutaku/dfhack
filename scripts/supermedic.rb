56fa9b0e005024ab969db3178deb1cb734ac0451
rusutaku <rustaveli-webservice@memoad.jp>
2012-11-08 00:46:19 +0900

    supermedic.rb: Add "rest" (command 'Rest' job) option, and some minor changes. the "rest" option will be useful for "ignored by doctor" dwarf(http://www.bay12games.com/dwarves/mantisbt/view.php?id=94). now "wake" function makes "Clean Self" instead of "No Job".  「仕事」がメモリに残ってるんじゃないか心配なのでとりあえず．

--- scripts/supermedic.rb
+++ scripts/supermedic.rb
@@ -63,10 +63,17 @@ wakeup = lambda { |u, f|
     return unless u.job.current_job
     jobtype = u.job.current_job.job_type
     if f or jobtype == :Rest
-        u.job.current_job = nil
+        u.job.current_job.job_type = :CleanSelf
         u.counters.unconscious = 0
         puts "released from #{jobtype} job."
     end
+}
+
+rest = lambda { |u|
+    j = DFHack::Job.cpp_new
+    u.job.current_job = j
+    u.job.current_job.job_type = :Rest
+    puts "commanded Rest job."
 }
 
 clear_syndromes = lambda { |u, f|
@@ -99,38 +106,43 @@ repair_him = lambda { |u, force|
                 clear_syndromes[u, force]
             when "wake"
                 wakeup[u, force]
+            when "rest"
+                rest[u]
             end
         }
     end
 }
 
 if args.include?('man') or args.include?('help') or args.include?('?')
-    puts "Repair him/her. Use this when you see the 'unmovable dwarves' hospital bugs."
+    puts "Some workarounds for health care bugs."
     puts "Please select a unit or select by following target option."
     puts "Options(target):"
     puts "  nick:x - execute repair function(s) to that nicknamed creature(s)."
     puts "           i.e. nick:foo means select all creature(s) nicknamed as 'foo'."
     puts "           you can use spaces by using quotes."
-    puts "Options(repair):"
-    puts "  all       - execute all following repair functions to the patient."
-    puts "              no options is the same as execute all."
+    puts "Options(functions):"
+    puts "  all       - execute all following repair functions except 'rest'."
+    puts "              no options is the same as this."
     puts "  wounds    - clear all wounds"
     puts "  reqs      - clear all treatment requests"
     puts "  stand     - force walkable (also impair)"
     puts "  grasp     - force graspable (also impair)"
     puts "  syndromes - clear all syndromes"
     puts "  wake      - release from 'Rest' job"
+    puts "  rest      - command 'Rest' job"
     puts "Force Option:"
     puts "  -f or --force - force execute repair function(s)"
     puts ""
     puts "Usage:"
     puts "  supermedic wounds reqs stand wake"
     puts "    - repair the selecting patient without grasp"
-    puts ""
-    puts "  supermedic nick:""Ignored Dwarf"""
-    puts "    - do all repairs to nicknamed as ""Ignored Dwarf"""
+    puts "  supermedic nick:""Nick"""
+    puts "    - do all repairs to nicknamed as ""Nick"""
     puts "  supermedic wake -f"
     puts "    - force unstuck from current job."
+    puts "  supermedic rest"
+    puts "    - this will be useful for 'ignored by doctor' dwarf."
+    puts "      ref. http://www.bay12games.com/dwarves/mantisbt/view.php?id=94"
 else
     nick = ""
     patients = []
